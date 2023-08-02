ad203bce30da300e5996dcdd97614ef6355a0c65
YiLinTay1 <yilintay1@gmail.com>
2023-08-02 13:25:56 +0100

    -

--- dist/script.js
+++ dist/script.js
@@ -1,120 +1,14 @@
-function redirectToInformationPage(clickedImage) {
-  // Extract the data from the image's data attributes
-  const imageSrc = clickedImage.getAttribute('src');
-  const accessionNo = clickedImage.getAttribute('data-accession-no');
-  const mitchellLibraryNo = clickedImage.getAttribute('data-mitchell-library-no');
-  const category = clickedImage.getAttribute('data-category');
-  const title = clickedImage.getAttribute('data-title');
-  const location = clickedImage.getAttribute('data-location');
-  const technique = clickedImage.getAttribute('data-technique');
-  const notes = clickedImage.getAttribute('data-notes');
-  const date = clickedImage.getAttribute('data-date');
-  const maplink = clickedImage.getAttribute('map-link');
-  const arDesc = clickedImage.getAttribute('data-ar-description');
-  sessionStorage.setItem('desc', arDesc);
+// Function for onclick images in collection page, redirect to individual collection page with respective information
+function redirectToInformationPage(clickedImage) {      
+  // Extract the data-record-number attribute from the clicked image
+  const mitchellLibraryNo = clickedImage.getAttribute('mitchell-library-no');
 
-  // Construct the URL of the target page with query parameters
-  const targetPageUrl = `collection_individual.html?imageSrc=${encodeURIComponent(imageSrc)}&accessionNo=${accessionNo}&mitchellLibraryNo=${mitchellLibraryNo}&category=${category}&title=${title}&location=${location}&technique=${technique}&notes=${notes}&date=${date}&mapLink=${maplink}`;
+  // Construct the URL of the target page with the data-record-number parameter
+  const targetPageUrl = `collection_individual.html?mitchellLibraryNo=${encodeURIComponent(mitchellLibraryNo)}`;
 
   // Redirect the user to the target page
   window.location.href = targetPageUrl;
 }
-
-// function initGoogleSheetsAPI() {
-//   console.log('Initializing Google Sheets API...');
-//   gapi.client.init({
-//     apiKey: 'AIzaSyBAs9z4X4aXpiC28b_vbG2ttQakqNsHReY',
-//     discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
-//   })
-//   .then(() => {
-//     console.log('Google Sheets API initialized.');
-//   })
-//   .catch((error) => {
-//     console.error('Error initializing Google Sheets API:', error);
-//   });
-// }
-
-
-// // Load the Google Sheets API client library
-// gapi.load('client', initGoogleSheetsAPI);
-
-// // Accession No.	Mitchell Library No.	Category	Title	Location	Technique	Notes	Date	Map
-// async function getImageInformation(image) {
-//   console.log('getImageInformation called'); // Add this line
-//   const recordNumber = image.getAttribute('data-record-number');
-
-//   try {
-//     const response = await gapi.client.sheets.spreadsheets.values.get({
-//       spreadsheetId: '12v30ruxwYpkmxIYUNuySa9TZCwWTLeHN',
-//       range: `Sheet1!A3:I`, // Start from row 3 onwards
-//     });
-
-//     const data = response.result.values;
-//     const record = data.find(row => row[0] === recordNumber);
-
-//     if (record) {
-//       // Extract the data from the image's data attribute
-//       const imageSrc = clickedImage.getAttribute('src');
-//       const accessionNo = record[0];
-//       const libraryNo = record[1];
-//       const category = record[2];
-//       const title = record[3];
-//       const location = record[4];
-//       const technique = record[5];
-//       const notes = record[6];
-//       // const description = clickedImage.getAttribute('description');
-//       const date = record[7];
-//       const maplink = record[8];
-
-//       // Construct the URL of the target page with query parameters
-//       // const targetPageUrl = `collection_individual.html?title=${title}&date=${date}&accessionNo=${accessionNo}&libraryNo=${libraryNo}&technique=${technique}&notes=${notes}&location=${location}&imageSrc=${encodeURIComponent(imageSrc)}&mapLink=${maplink}&description=${encodeURIComponent(description)}`; // Updated
-//       const targetPageUrl = `collection_ind.html?imageSrc=${encodeURIComponent(imageSrc)}&accessionNo=${accessionNo}&libraryNo=${libraryNo}&category=${category}&title=${title}&location=${location}&technique=${technique}&notes=${notes}&date=${date}&mapLink=${maplink}`; // Updated
-
-//       // Redirect the user to the target page
-//       window.location.href = targetPageUrl;
-
-//     } else {
-//       // If no matching record found
-//       console.log('Record not found.');
-//     }
-//   } catch (error) {
-//     console.error('Error accessing Google Sheets:', error);
-//   }
-// }
-
-// async function getImageInformation(image) {
-//   const recordNumber = image.getAttribute('data-record-number');
-
-//   try {
-//     const response = await gapi.client.sheets.spreadsheets.values.get({
-//       spreadsheetId: '1P-huAtEuSAkE6gI_I-OWBCVHlvdzYxS_jbcAmqoYsoI',
-//       range: `Sheet1!A2:H`, // Assuming your data starts from row 2 in the spreadsheet
-//     });
-
-//     const data = response.result.values;
-//     const record = data.find(row => row[0] === recordNumber);
-
-//     if (record) {
-//       const caption = record[1];
-//       const description = record[2];
-//       const area = record[3];
-//       const street = record[4];
-//       const number = record[5];
-//       const date = record[6];
-//       const mlLink = record[7];
-//       const targetPageUrl = `collection_ind.html?caption=${caption}&date=${date}&recordNumber=${recordNumber}&area=${area}&street=${street}&number=${number}&mlLink=${mlLink}&description=${encodeURIComponent(description)}`; // Updated
-
-//       // Redirect the user to the target page
-//       window.location.href = targetPageUrl
-//     } else {
-//       // If no matching record found
-//       console.log('Record not found.');
-//     }
-//   } catch (error) {
-//     console.error('Error accessing Google Sheets:', error);
-//   }
-// }
-
 
 // Function to close the pop-up
 function closePopup() {
